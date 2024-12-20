cbb83a8b2dcd877c019142adf98f197b6a0f8c9c
evseer <mynameisevse@gmail.com>
2024-12-20 20:45:40 +0300

    Lesson 1: Repository initialization

---
+++ Main.java
@@ -0,0 +1,12 @@
+import lesson_1.task_1;
+import lesson_2.task_2;
+
+public class Main {
+    public static void main(String[] args){
+//    Lesson_1
+//    task_1.execute();
+
+//     Lesson_2
+    task_2.execute();
+    }
+}
---
+++ lesson_1/task_1.java
@@ -0,0 +1,7 @@
+package lesson_1;
+
+public class task_1 {
+    public static void execute() {
+        System.out.println("Lesson_1: Repository created and initialized successfully.");
+    }
+}
