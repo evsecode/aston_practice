86129eed2f8c70a6ae25d17969d92b1532efcc64
evseer <mynameisevse@gmail.com>
2025-02-10 23:23:42 +0300

    Lesson 13: Implemented factorial calculator and added unit tests using JUnit 5

---
+++ src/main/java/lesson_13/Main.java
@@ -0,0 +1,8 @@
+package lesson_13;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 4;
+        System.out.println("Factorial of " + number + " is " + FactorialCalculator.calculate(number));
+    }
+}
