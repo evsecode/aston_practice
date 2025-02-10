51dcc293dd851dc4ded36abb4ceb19da8d41dd83
evseer <mynameisevse@gmail.com>
2025-02-10 23:28:29 +0300

    Lesson 13: Implemented factorial calculator and added unit tests using TestNG

---
+++ src/main/java/lesson_13/FactorialCalculator.java
@@ -0,0 +1,14 @@
+package lesson_13;
+
+public class FactorialCalculator {
+    public static int calculate(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+        }
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
